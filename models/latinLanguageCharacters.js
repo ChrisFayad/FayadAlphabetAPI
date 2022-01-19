59e6a50c76c9bcf335d39eebb271fb62b05e7f68
Chris <fayadchristina@gmail.com>
2022-01-19 14:44:23 +0100

    Adding Mongoose Validation for Unique Language Value

--- models/latinLanguageCharacters.js
+++ models/latinLanguageCharacters.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
-const LanguageCharactersSchema = mongoose.Schema({
+const latinLanguageCharactersSchema = mongoose.Schema({
     language: {
         type: String,
+        unique: true,
         required: true,
-        // validate: {
-        //     validator: async (language) => await LanguageCharacters.where({ language }).countDocuments() === 0,
-        //     message: language => `The ${language.value} language has already been added.`
-        // }
+        trim: true
     },
     capsAlphabet: {
         type: [],
-        required: true
+        required: true,
+        trim: true,
     },
     primaryCapsAlphabet: [{
         char: String,
@@ -23,7 +22,8 @@ const LanguageCharactersSchema = mongoose.Schema({
     }],
     lowerAlphabet: {
         type: [],
-        required: true
+        required: true,
+        trim: true,
     },
     primaryLowerAlphabet: [{
         char: String,
@@ -35,22 +35,27 @@ const LanguageCharactersSchema = mongoose.Schema({
     }],
     firstKeypadLayer: {
         type: [],
+        trim: true,
         default: ["7", "8", "9", "4", "5", "6", "1", "2", "3", "*", "0", "#"]
     },
     secondKeypadLayer: {
         type: [],
+        trim: true,
         default: ["002E", "0020", "0026", "227A", "227B", "232B", "002B", "2212", "003D", "00B0", "002F", "000A"]
     },
     thirdKeypadLayer: {
         type: [],
+        trim: true,
         default: ["0025", "005F", "0027", "0028", "0029", "0024", "0022", "003A", "20AC", "002C", "003B", "00A3"]
     },
     fourthKeypadLayer: {
         type: [],
+        trim: true,
         default: ["005C", "007C", "007E", "003C", "003E", "005E", "005B", "005D", "00A5", "007B", "007D", "0060"]
     },
     specialSymbols:{
         type: [],
+        trim: true,
         default: ["!", "@", "?"]
     },
     specialSymbolsObject: [{
@@ -59,6 +64,7 @@ const LanguageCharactersSchema = mongoose.Schema({
     }],
     numberAbbreviations:{
         type: [],
+        trim: true,
         default: ["K", "M", "B", "T"]
     },
     numberAbbreviationsObject: [{
@@ -67,4 +73,9 @@ const LanguageCharactersSchema = mongoose.Schema({
     }],
 });
 
-module.exports = mongoose.model('LanguageCharacters', LanguageCharactersSchema);
+latinLanguageCharactersSchema.path('language').validate(async (language) => {
+    const languageCount = await mongoose.models.LatinLanguageCharacters.countDocuments({ language });
+    return !languageCount;
+}, `The language you are trying to insert already exists!`);
+
+module.exports = mongoose.model('LatinLanguageCharacters', latinLanguageCharactersSchema);
--- models/nonLatinLanguageCharacters.js
+++ models/nonLatinLanguageCharacters.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const nonLatinLanguageCharactersSchema = mongoose.Schema({
     language: {
         type: String,
-        required: true
+        unique: true,
+        required: true,
+        trim: true
     },
     Alphabet: {
         type: [],
-        required: true
+        required: true,
+        trim: true,
     },
     primaryAlphabet: [{
         char: String,
@@ -19,22 +22,27 @@ const nonLatinLanguageCharactersSchema = mongoose.Schema({
     }],
     firstKeypadLayer: {
         type: [],
+        trim: true,
         default: ["٧", "٨", "٩", "٤", "٥", "٦", "١", "٢", "٣", "*", "٠", "#"],
     },
     secondKeypadLayer: {
         type: [],
+        trim: true,
         default: ["002E", "0020", "0026", "227A", "227B", "232B", "002B", "2212", "003D", "00B0", "002F", "000A"]
     },
     thirdKeypadLayer: {
         type: [],
+        trim: true,
         default: ["066A", "005F", "0027", "0028", "0029", "0024", "0022", "003A", "20AC", "060C", "061B", "00A3"],
     },
     fourthKeypadLayer: {
         type: [],
+        trim: true,
         default: ["005C", "007C", "007E", "003C", "003E", "005E", "005B", "005D", "00A5", "007B", "007D", "0060"]
     },
     specialSymbols:{
         type: [],
+        trim: true,
         default: ["!", "@", "؟"]
     },
     specialSymbolsObject: [{
@@ -43,6 +51,7 @@ const nonLatinLanguageCharactersSchema = mongoose.Schema({
     }],
     numberAbbreviations:{
         type: [],
+        trim: true,
         default: ["ألف", "مليون", "مليار", "تريليون"]
     },
     numberAbbreviationsObject: [{
@@ -50,5 +59,10 @@ const nonLatinLanguageCharactersSchema = mongoose.Schema({
         code: Number
     }],
 });
+
+nonLatinLanguageCharactersSchema.path('language').validate(async (language) => {
+    const languageCount = await mongoose.models.nonLatinLanguageCharacters.countDocuments({ language });
+    return !languageCount;
+}, `The language you are trying to insert already exists!`);
 
 module.exports = mongoose.model('nonLatinLanguageCharacters', nonLatinLanguageCharactersSchema);
