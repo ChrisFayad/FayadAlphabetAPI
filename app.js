ae7c7e6d9b8636a4ddbc80ca74267c2c1624613a
Chris <fayadchristina@gmail.com>
2022-01-25 16:10:18 +0100

    Remove express-validation package

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv/config');
 const languagesRoutes = require('./routes/languages');
-const { ValidationError } = require('express-validation');
 
 mongoose.connect(process.env.DB_CONNECTION);
 
