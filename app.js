67c4ee10bf3e03478ea71cb558e87cee21d2af39
Chris <fayadchristina@gmail.com>
2022-01-25 22:56:18 +0100

    Using SSL instead of http

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv/config');
 const languagesRoutes = require('./routes/languages');
 
-mongoose.connect(process.env.DB_CONNECTION);
+mongoose.connect(process.env.USER_DB_CONNECTION);
 
 const app = express();
 const PORT = 7000 || process.env.PORT;
@@ -12,4 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/languages', languagesRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
+};
+
+https.createServer(options, app).listen(PORT, () => console.log(`Server is running on port ${PORT}`));
