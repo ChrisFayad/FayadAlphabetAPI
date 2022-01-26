2acaebe4ea8872af0603902ebcf148eab527e8f8
Chris <fayadchristina@gmail.com>
2022-01-26 11:20:48 +0100

    Adding cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 const languagesRoutes = require('./routes/languages');
 const apiUsersRoutes = require('./routes/apiUsers');
@@ -12,8 +13,8 @@ const PORT = 7000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/languages', languagesRoutes);
-app.use('/developers', apiUsersRoutes);
+app.use('/languages', cors(), languagesRoutes);
+app.use('/developers', cors({origin: 'https://localhost:7000'}), apiUsersRoutes);
 
 const options = {
     key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
